015433ad48396862df968d3663bcb472308fdab6
Ashutosh Mishra <mishratechie@users.noreply.github.com>
2016-08-26 12:06:40 +0530

    Added Long generator

--- src/com/ashu/RandomNumberGenerator.java
+++ src/com/ashu/RandomNumberGenerator.java
@@ -19,11 +19,21 @@ public class RandomNumberGenerator {
 	  
 	        return Integer.parseInt(number);  
 	  
-	    }  
+	    }
+	  public static Long getRandomLong(int length){
+
+	        Random r = new Random();
+	        String number="";
+	        int counter=0;
+	        while(counter++< length) number+= r.nextInt(9);
+	
+	        return Long.parseLong(number);
+    	 }
 	  
 	    public static void main(String[] args) {  
 	     
-	        System.out.println(getRandomNumber(8));  
+	        System.out.println(getRandomNumber(8));
+	        System.out.println(getRandomLong(15));  
 	  
 	    }  
 	
